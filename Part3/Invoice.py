7db6744fa14db7dd03fefece605ce7c58cb1e72c
K <kad9603@gmail.com>
2021-03-01 18:27:05 -0500

    Part3 complete

--- Part3/Invoice.py
+++ Part3/Invoice.py
@@ -14,20 +14,16 @@ class Invoice:
 
     def totalImpurePrice(self, products):
         total_impure_price = 0
-
         for k, v in products.items():
-            total_impure_price += float(v['unit_price']) * float(v['qnt'])
-
+            total_impure_price += float(v['unit_price']) * int(v['qnt'])
         total_impure_price = round(total_impure_price, 2)
         return total_impure_price
 
 
     def totalDiscount(self, products):
         total_discount = 0
-
         for k, v in products.items():
-            total_discount += (float(v['qnt']) * float(v['discount'])) / 100
-
+            total_discount += (int(v['qnt']) * float(v['unit_price']) * float(v['discount'])) / 100
         total_discount = round(total_discount, 2)
         self.total_discount = total_discount
         return total_discount
